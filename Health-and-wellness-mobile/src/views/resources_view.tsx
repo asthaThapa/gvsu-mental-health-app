44481f0af76aa1c30ab161177ce1605d59ca8ddb
asthaThapa <aastha0914@gmail.com>
2024-02-12 15:47:45 -0500

    Resources Icons update

--- Health-and-wellness-mobile/src/views/resources_view.tsx
+++ Health-and-wellness-mobile/src/views/resources_view.tsx
@@ -17,7 +17,25 @@ import ResourceCard from '../components/card_resources';
 import { ResourceCardInfo } from '../stores/models/data_models';
 
 import imgPlaceholder from '../assets/gv_placeholder_logo.jpg';
+import environment from '../assets/icons/resources/resourceEnv.png'
+import occupational from '../assets/icons/resources/resourceOcc.png'
+import emotional from '../assets/icons/resources/resourceEmo.png'
+import physical from '../assets/icons/resources/resourcePhy.png'
+import social from '../assets/icons/resources/resourceSoc.png'
+import financial from '../assets/icons/resources/resourceFin.png'
+import spirtual from '../assets/icons/resources/resourceSpi.png'
+import intellectual from '../assets/icons/resources/resourceInt.png'
 
+const mainResImages = [
+  { name: 'resourceEnv', image: environment },
+  { name: 'resourceOcc', image: occupational },
+  { name: 'resourceEmo', image: emotional },
+  { name: 'resourcePhy', image: physical },
+  { name: 'resourceSoc', image: social },
+  { name: 'resourceFin', image: financial },
+  { name: 'resourceSpi', image: spirtual },
+  { name: 'resourceInt', image: intellectual },
+];
 
 export interface Props {
   store: Store
@@ -76,12 +94,13 @@ export default class ResourcesView extends React.Component<Props> {
   }
 
   private renderCard(item: ResourcesCardTile) {
+    var iconImg = mainResImages.find(resource => resource.name === item.info.img)?.image || imgPlaceholder;
     return (
       <ResourceCard
         open={item.open}
         enableModal={true}
         title={item.info.title}
-        iconImage={imgPlaceholder}
+        iconImage={iconImg}
         onToggleOpen={this.handleToggleModal(item)}
       >
         {this.renderResources(item.info.subResources)}
